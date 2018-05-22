3f2b607c5dc1c63451d9c09351ea5a4d456f8bb0
Oliver Dudman <oliver.dudman@icloud.com>
2018-05-22 11:52:24 +0100

    fix cell does not exsist position error

--- app/src/board.js
+++ app/src/board.js
@@ -6,6 +6,7 @@ class Board extends React.Component {
 
     this.cellSize = 10;
     this.cellMargin = 2;
+    this.cellTotal = this.cellSize + this.cellMargin;
 
     this.drawSquare = this.drawSquare.bind(this);
     this.handleClick = this.handleClick.bind(this);
@@ -15,8 +16,8 @@ class Board extends React.Component {
     let drawSquare = this.drawSquare;
     let c = document.getElementById("board-canvas");
     let ctx = c.getContext("2d");
-    let canvasWidth = this.props.size.w * (this.cellSize + this.cellMargin);
-    let canvasHeight = this.props.size.h * (this.cellSize + this.cellMargin);
+    let canvasWidth = this.props.size.w * (this.cellTotal);
+    let canvasHeight = this.props.size.h * (this.cellTotal);
     ctx.clearRect(0, 0, canvasWidth, canvasHeight);
 
     let cells = this.props.activeCells;
@@ -31,22 +32,22 @@ class Board extends React.Component {
   }
 
   drawSquare(i, j, ctx, cellValue) {
-    let positionX = i * (this.cellSize + this.cellMargin);
-    let positionY = j * (this.cellSize + this.cellMargin);
+    let positionX = i * (this.cellTotal);
+    let positionY = j * (this.cellTotal);
     let cellColor = cellValue === 1 ? "orange" : "red";
     ctx.fillStyle = cellColor;
     ctx.fillRect(positionX, positionY, this.cellSize, this.cellSize);
   }
 
   handleClick(e) {
-    let i = Math.floor(e.nativeEvent.offsetX / (this.cellSize + this.cellMargin));
-    let j = Math.floor(e.nativeEvent.offsetY / (this.cellSize + this.cellMargin));
+    let i = Math.min(this.props.size.w - 1, Math.floor(e.nativeEvent.offsetX / (this.cellTotal)));
+    let j = Math.min(this.props.size.h - 1, Math.floor(e.nativeEvent.offsetY / (this.cellTotal)));
     this.props.handleCellClick({i: i, j: j});
   }
 
   render() {
-    let canvasWidth = this.props.size.w * (this.cellSize + this.cellMargin);
-    let canvasHeight = this.props.size.h * (this.cellSize + this.cellMargin);
+    let canvasWidth = this.props.size.w * (this.cellTotal);
+    let canvasHeight = this.props.size.h * (this.cellTotal);
 
     return (
       <canvas id="board-canvas" onClick={this.handleClick} width={canvasWidth} height={canvasHeight} style={{backgroundColor: "grey", padding: "2px"}}></canvas>
